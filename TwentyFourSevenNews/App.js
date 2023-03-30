9d4bbe14479140835e26848957ea7e7cedfa84f5
MRRehman <moizurrehman12345@gmail.com>
2023-03-30 20:14:10 +0500

    Adding react native project

---
+++ TwentyFourSevenNews/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+	<TextInput
+	style={{height: 40}}
+        placeholder="Type here!"
+	/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
